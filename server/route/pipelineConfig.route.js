3f85f0916041b2d7070636f3699239974b61e451
harsha3011 <harsha.9204@gmail.com>
2017-01-10 08:20:06 +0530

    all files

--- server/route/pipelineConfig.route.js
+++ server/route/pipelineConfig.route.js
@@ -1,6 +1,6 @@
 const pipelineConfig=require('../services/createPipelineConfig');
 const express = require('express')
 const router = express.Router()
-router.post('/repo/:repoId/pipeline',pipelineConfig);
+router.put('/repo/:repoId/pipeline',pipelineConfig);
 
 module.exports=router;
