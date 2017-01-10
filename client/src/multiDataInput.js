463c91e9abe0ad400012472db481a707405cfc5e
Vaibhaw Singh <vaibhawsingh92@gmail.com>
2017-01-10 08:41:25 +0530

    intial commit

--- client/src/multiDataInput.js
+++ client/src/multiDataInput.js
@@ -1,78 +1,155 @@
 import Chip from 'material-ui/Chip';
-import RaisedButton from 'material-ui/RaisedButton';
+import FlatButton from 'material-ui/FlatButton';
 import TextField from 'material-ui/TextField';
 import React,{Component} from 'react';
+import ContentAdd from 'material-ui/svg-icons/content/add';
 class MultiInputComp extends Component
 {
-  constructor(props) {
-    super(props);
-    this.state = {chipData: [],
-    count:1,
-    lint:''};
-  }
-
-  handleRequestDelete = (key) => {
-    this.chipData = this.state.chipData;
-    const chipToDelete = this.chipData.map((chip) => chip.key).indexOf(key);
-    this.chipData.splice(chipToDelete, 1);
-    this.setState({chipData: this.chipData});
-    this.props.makeChange(this.state.htmlData);
-  };
-
-  renderChip(data) {
-    return (
-      <Chip style={{margin:5}}
-        key={data.key}
-        onRequestDelete={() => this.handleRequestDelete(data.key)}
-        >
-        {data.label}
-      </Chip>
-
-    );
-  }
-  handleRequestAdd=(e)=>
-  {
-
-    this.chipData = this.state.chipData;
-    var k=this.state.count;
-    var obj={
-      key: ++k,
-      label:this.state.lint
+  constructor() {
+    super();
+    this.state = {
+      inputValue: [],
+      index:-1
     }
-    this.setState(
-      {
-        lint:'',
-        count:k
-      }
-    );
-    this.chipData.push(obj);
-    this.setState({chipData: this.chipData});
-      this.props.makeChange(this.state.chipData);
-    this.state.chipData.map(this.renderChip, this);
   }
-  changeText=(e)=>
-  {
-    this.setState({
-      lint:e.target.value
-    })
+
+  static get propTypes() {
+    return {
+      values: React.PropTypes.array.isRequired,
+      onChange: React.PropTypes.func.isRequired
+    }
   }
+
   render() {
-    return (
-      <div >
-            <TextField style={{margin:20}}
-          floatingLabelText="Enter file names"
-          onChange={this.changeText}
-          value={this.state.lint}
+    const inputField = (
+      <TextField
+        floatingLabelText="Enter Filenames"
+        value={this.state.inputValue}
+        onChange={this.handleInputValueChange.bind(this)} />
+    );
+    const addButton = (
+      <FlatButton
+        icon={<ContentAdd />}
+        onTouchTap={this.handleAddValue.bind(this)}
         />
-        <RaisedButton label="Add"
-         onClick={this.handleRequestAdd}
-         />
-        <br />
-        <div style={{display:'flex', flexWrap:'wrap'}}>
-        {this.state.chipData.map(this.renderChip, this)}
-        </div>
+    )
+    const values = this.props.values.map((value, index) => {
+
+      return (
+        <Chip onRequestDelete={this.handleRemoveValue.bind(this, index)}
+        >{value.data}</Chip>
+      );
+    });
+    return (
+      <div>
+      {inputField}
+      {addButton}
+      {values}
       </div>
     );
+  }
+
+  handleInputValueChange(event) {
+    const newInputValue = event.target.value;
+    this.setState({inputValue: newInputValue});
+  }
+
+  handleAddValue() {
+    var key=this.state.index;
+    key++;
+    const values = this.props.values;
+    var obj={key:key,
+    data:this.state.inputValue}
+    values.push(obj);
+    this.setState({index: key,
+    inputValue:''});
+    this.props.onChange(values);
+
+  }
+
+  handleRemoveValue(valueIndex) {
+    const values = this.props.values;
+    const valueToDelete = values.map((data) => data.key).indexOf(valueIndex);
+    values.splice(valueToDelete,1);
+    this.props.onChange(values);
+  }
+}
+
+export default MultiInputComp;
+import Chip from 'material-ui/Chip';
+import FlatButton from 'material-ui/FlatButton';
+import TextField from 'material-ui/TextField';
+import React,{Component} from 'react';
+import ContentAdd from 'material-ui/svg-icons/content/add';
+class MultiInputComp extends Component
+{
+  constructor() {
+    super();
+    this.state = {
+      inputValue: [],
+      index:-1
+    }
+  }
+
+  static get propTypes() {
+    return {
+      values: React.PropTypes.array.isRequired,
+      onChange: React.PropTypes.func.isRequired
+    }
+  }
+
+  render() {
+    const inputField = (
+      <TextField
+        floatingLabelText="Enter Filenames"
+        value={this.state.inputValue}
+        onChange={this.handleInputValueChange.bind(this)} />
+    );
+    const addButton = (
+      <FlatButton
+        icon={<ContentAdd />}
+        onTouchTap={this.handleAddValue.bind(this)}
+        />
+    )
+    const values = this.props.values.map((value, index) => {
+
+      return (
+        <Chip onRequestDelete={this.handleRemoveValue.bind(this, index)}
+        >{value.data}</Chip>
+      );
+    });
+    return (
+      <div>
+      {inputField}
+      {addButton}
+      {values}
+      </div>
+    );
+  }
+
+  handleInputValueChange(event) {
+    const newInputValue = event.target.value;
+    this.setState({inputValue: newInputValue});
+  }
+
+  handleAddValue() {
+    var key=this.state.index;
+    key++;
+    const values = this.props.values;
+    var obj={key:key,
+    data:this.state.inputValue}
+    values.push(obj);
+    this.setState({index: key,
+    inputValue:''});
+    this.props.onChange(values);
+
+  }
+
+  handleRemoveValue(valueIndex) {
+    const values = this.props.values;
+    const valueToDelete = values.map((data) => data.key).indexOf(valueIndex);
+    values.splice(valueToDelete,1);
+    this.props.onChange(values);
   }
 }
 
--- client/src/testSettings.js
+++ client/src/testSettings.js
@@ -18,7 +18,7 @@ import _ from 'lodash';
 import Request from 'superagent';
 import {Tabs, Tab} from 'material-ui/Tabs';
 import Slider from 'material-ui/Slider';
-import MultiInputComp from './multiDataInput';
+import LintTest from './lintTest';
 const muiTheme = getMuiTheme({
  palette: {
    textColor: white,
@@ -33,57 +33,14 @@ class TestSettings extends Component{
         super(props);
         this.handleClick=this.handleClick.bind(this)
         this.handleChange=this.handleChange.bind(this)
-        this.htmlChange=this.htmlChange.bind(this)
-        this.esChange=this.esChange.bind(this)
-        this.testChange=this.testChange.bind(this)
+
         this.state = {
-          htmlData:[],
-          esData:[],
-          exHtmlData:[],
-          exEsData:[],
-          testData:[],
-          exTestData:[],
+
           value: '',
           add:false,
         };
       }
-      htmlChange(htmlData)
-      {
-        this.setState={
-          htmlData:htmlData
-        }
-      }
-      exHtmlChange(exHtmlData)
-        {
-          this.setState={
-            exHtmlData:exHtmlData
-          }
-        }
-        exEsChange(exEsData)
-          {
-            this.setState={
-              exEsData:exEsData
-            }
-          }
-          exTestChange(exTestData)
-            {
-              this.setState={
-                exTestData:exTestData
-              }
-            }
-      testChange(testData)
-      {
-        this.setState={
-          testData:testData
-        }
-      }
-      esChange(esData)
-      {
-        this.setState={
-          esData:esData
-        }
-      }
-
+      
       handleChange =(event)=>{
          this.setState({
           add:true,
@@ -150,67 +107,22 @@ npm install
         </Grid>
         <Grid>
           <Row>
-            <Col xs={12}>
+            <Col>
               <Row start="xs">
                 <Col xs={12}>
                  <Tabs>
                   <Tab label="LINTING" >
               <Paper>
                <List>
+               <Row>
+                 <Col xs={12}>
                  <ListItem>
-                  <Card>
-                    <CardHeader
-                     title="EsLint"
-                      actAsExpander={true}
-                      showExpandableButton={true}
-                    >
-                    </CardHeader>
-                    <CardText expandable={true}>
-                    <Row>
-                      <Col xs={12}>
-                        <Row center="xs">
-                          <Col xs={6}>
-                            Include files to test
-                            <MultiInputComp data={this.state.esData}
-                            makeChange={this.esChange}/>
-                          </Col>
-                          <Col xs={6}>
-                            Exclude files from test
-                            <MultiInputComp data={this.state.exEsData}
-                            makeChange={this.exEsChange}/>
-                          </Col>
-                        </Row>
-                      </Col>
-                    </Row>
-                    </CardText>
-                  </Card>
+                      <LintTest/>
                 </ListItem>
+                </Col>
+                </Row>
                  <ListItem>
-                  <Card>
-                    <CardHeader
-                     title="HTML Lint"
-                      actAsExpander={true}
-                      showExpandableButton={true}
-                    />
-                    <CardText expandable={true}>
-                    <Row>
-                      <Col xs={12}>
-                        <Row center="xs">
-                          <Col xs={6}>
-                            Include files to test
-                            <MultiInputComp data={this.state.htmlData}
-                            makeChange={this.htmlChange}/>
-                          </Col>
-                          <Col xs={6}>
-                            Exclude files from test
-                            <MultiInputComp data={this.state.exHtmlData}
-                            makeChange={this.exHtmlChange}/>
-                          </Col>
-                        </Row>
-                      </Col>
-                    </Row>
-                    </CardText>
-                  </Card>
+                  <LintTest/>
                 </ListItem>
                 </List>
                   </Paper>
@@ -219,30 +131,7 @@ npm install
                     <Paper>
                      <List>
                  <ListItem>
-                  <Card>
-                    <CardHeader
-                      title="Automated Testing"
-                      actAsExpander={true}
-                      showExpandableButton={true}
-                    />
-                    <CardText expandable={true}>
-                    <Row>
-                      <Col xs={12}>
-                        <Row center="xs">
-                          <Col xs={6}>
-                    Include files to test
-                    <MultiInputComp data={this.state.testData}
-                    makeChange={this.testChange}/>
-                      </Col>
-                      <Col xs={6}>
-                    Exclude files from test
-                    <MultiInputComp data={this.state.exTestData}
-                    makeChange={this.exTestChange}/>
-                    </Col>
-                    </Row>
-                    </Col></Row>
-                    </CardText>
-                  </Card>
+                 <LintTest/>
                 </ListItem>
                 <ListItem>
                   <Card>
