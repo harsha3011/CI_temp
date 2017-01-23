ae6545c395f8993ad23ab4a874cfc18288272ff4
PaurviBhardwaj <paurvi2105@gmail.com>
2017-01-23 14:42:22 +0530

    my commit

--- server/dockerExecution.sh
+++ server/dockerExecution.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+
 git clone $REPO_URL -b $REPO_BRANCH
 
 ROOT_DIR=`pwd`
@@ -15,25 +16,7 @@ npm install
 
 npm install --save eslint-config-google eslint-json
 
-echo htmlhint $HTMLHINT
-htmlhint -f json $HTMLHINT > $ROOT_DIR/outputJson/htmlhintOutput.json
+node $ROOT_DIR/runTestcases.js $MOCHA $ESLINT $ISTANBUL $HTMLHINT $REPO_NAME $REPO_BRANCH $OWNER
+cd ..
 
-echo eslint $ESLINT
-eslint --format=node_modules/eslint-json $ESLINT > $ROOT_DIR/outputJson/eslintOutput.json
-
-echo mocha $MOCHA
-if (($MOCHA))
-then
-mocha $MOCHA --reporter mochawesome --reporter-options reportDir=../outputJson,reportName=mochaOutput
-else
-mocha --reporter mochawesome --reporter-options reportDir=../outputJson,reportName=mochaOutput
-fi
-
-echo istanbul $ISTANBUL
-if (($ISTANBUL))
-then
-istanbul cover _mocha
-fi
- cd ..
-echo $STARTTIME
 node Save.js $REPO_NAME $REPO_BRANCH $OWNER $ISTANBUL $STARTTIME
